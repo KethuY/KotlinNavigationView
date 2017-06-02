fbeaef7783756cb323c82966ffc5ec9d18e59756
satya <kethusatya@gmail.com>
2017-06-02 14:34:14 +0530

    hj

--- app/src/main/java/com/satya/kotlinex/kotlinex/actvities/MainActivity.kt
+++ app/src/main/java/com/satya/kotlinex/kotlinex/actvities/MainActivity.kt
@@ -13,15 +13,12 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                mTextMessage!!.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                mTextMessage!!.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                mTextMessage!!.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -32,7 +29,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mTextMessage = findViewById(R.id.message) as TextView
         val navigation = findViewById(R.id.navigation) as BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
---
+++ app/src/main/java/com/satya/kotlinex/kotlinex/models/Customer.java
@@ -0,0 +1,8 @@
+package com.satya.kotlinex.kotlinex.models;
+
+/**
+ * Created by satya on 6/2/2017.
+ */
+
+public class Customer {
+}
